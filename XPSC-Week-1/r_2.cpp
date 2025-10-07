2376c312438ac10432f0b1cecf5227bf5f5d188d
RJannat <ramimk097@gmail.com>
2025-10-07 19:42:47 +0600

    solved another problem

---
+++ XPSC-Week-1/r_2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int r1, s, r2;
+    cin >> r1 >> s;
+    r2 = (s * 2) - r1;
+    cout << r2 << "\n";
+    return 0;
+}
