4688b9f001aaa4920524ddf795cf76961822ea42
RJannat <ramimk097@gmail.com>
2025-10-07 10:06:22 +0600

    unsolved another question

---
+++ XPSC-Week-1/A_Mahmoud_and_Ehab_and_the_MEX.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    int cnt = 0;
+    if (n == 1 && x == 0 && v[0] == 0)
+        cout << 1 << endl;
+    else
+    {
+        for (int i = 0; i <= x; i++)
+        {
+            bool f = true;
+            for (int val : v)
+            {
+                if (x == val)
+                {
+                    cnt++;
+                    break;
+                }
+                if (i == val && i != x)
+                {
+                    f = false;
+                    break;
+                }
+            }
+            if (f)
+                cnt++;
+        }
+        cout << cnt - 1 << endl;
+    }
+    return 0;
+}
